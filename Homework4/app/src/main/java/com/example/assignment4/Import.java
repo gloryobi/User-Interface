7c6e897ba04aa48b55df3162032aa3703bf649c7
Glory Obielodan <gloryobielodan@gmail.com>
2018-10-17 18:33:45 -0500

    Commiting classwork for CSCI5115

---
+++ Homework4/app/src/main/java/com/example/assignment4/Import.java
@@ -0,0 +1,31 @@
+package com.example.assignment4;
+
+/**
+ * Created by karanjaswani on 2/7/18.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Import extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        return inflater.inflate(R.layout.fragment_menu_import, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Import");
+    }
+}
